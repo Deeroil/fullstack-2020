33e3ded0d01857a0a6b9d2f4e47e89db2f4dc2be
Miina Saroma <miinasaroma@gmail.com>
2020-08-08 12:58:25 +0300

    Cleanup comments, format

--- osa2/kurssitiedot-2/src/App.js
+++ osa2/kurssitiedot-2/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Course from './components/Course'
 
-const App = ({courses}) => {
-    return (    
-      <div>
-        <h1>Web Dev Curriculum</h1>
-        {courses.map(course => <Course key={course.id} course={course} />)}
-      </div>
-    )
-  }
+const App = ({ courses }) => {
+  return (
+    <div>
+      <h1>Web Dev Curriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </div>
+  )
+}
 
-  export default App
+export default App
