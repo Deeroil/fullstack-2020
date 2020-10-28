94e11a70ec4513994f8e704844fa8f000e09fef8
Miina Saroma <miinasaroma@gmail.com>
2020-10-28 18:19:39 +0200

    Add tests for favorite blog

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,11 +6,23 @@ const totalLikes = (blogs) => {
     var total = 0
     blogs.forEach(blog => {
         total += blog.likes
-    });
+    })
     return total
+}
+
+const favoriteBlog = (blogs) => {
+    var mostLikes = blogs[0]
+
+    blogs.forEach(blog => {
+        if (blog.likes > mostLikes.likes) {
+            mostLikes = blog
+        }
+    })
+    return mostLikes
 }
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
