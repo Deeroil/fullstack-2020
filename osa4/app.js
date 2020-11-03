a87ed8eca427b0397b400b6f790c76a304ff3ccc
Miina Saroma <miinasaroma@gmail.com>
2020-11-04 00:26:56 +0200

    Fix app.js middleware order

--- osa4/app.js
+++ osa4/app.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
 const config = require('./utils/config')
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
 app.use(cors())
 app.use(express.json())
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 module.exports = app
