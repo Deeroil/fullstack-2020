d56fde52dcf2333932bfc58342373c1adb4acf3e
Miina Saroma <miinasaroma@gmail.com>
2020-11-04 19:51:59 +0200

    Remove __v property and replace _id with id

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -6,5 +6,14 @@ const blogSchema = mongoose.Schema({
     url: String,
     likes: Number
   })
+
+  //remove __v and replace _id with id
+  blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
   
   module.exports = mongoose.model('Blog', blogSchema)
