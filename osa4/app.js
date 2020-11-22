1dd9d9dbeb54d0975ffc23e1b57b0396febdd229
Miina Saroma <miinasaroma@gmail.com>
2020-11-22 11:55:53 +0200

    Add express-async-errors

--- osa4/app.js
+++ osa4/app.js
@@ -1,13 +1,17 @@
+const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-app.use(cors())
-app.use(express.json())
 const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
