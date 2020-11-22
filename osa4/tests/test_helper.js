1792a3b814c7846c42bf34826fc9fe53ef603940
Miina Saroma <miinasaroma@gmail.com>
2020-11-22 13:36:35 +0200

    Add test helper

---
+++ osa4/tests/test_helper.js
@@ -0,0 +1,27 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'Elämää fantasiaviidakossa',
+        author: 'Reetu Kirjoittaja',
+        url: 'vvv.fantasiawiiwakko.netti',
+        likes: '1496'
+    },
+    {
+        title: 'Daily lives of orangutans',
+        author: 'Aili Kananluoma',
+        url: 'vvv.orangos.netti',
+        likes: '506'
+
+    }
+]
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+    initialBlogs,
+    blogsInDb
+}
