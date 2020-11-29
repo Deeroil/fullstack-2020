1a7ea3ec3dfcef991a21cf49c453d660df938dae
Miina Saroma <miinasaroma@gmail.com>
2020-11-29 14:13:05 +0200

    Add tests for user creation

---
+++ osa4/tests/user_api.test.js
@@ -0,0 +1,68 @@
+const bcrypt = require('bcrypt')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+const helper = require('./test_helper')
+
+describe('with initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('password123', 10)
+        const user = new User({ username: 'root', user: 'Sir Bulbo', passwordHash })
+
+        await user.save()
+    })
+
+    test('api returns only the initial user', async () => {
+        const response = await api.get('/api/users')
+
+        expect(response.body).toHaveLength(1)
+        expect(response.body[0].username).toBe('root')
+    })
+
+    test('user with same name isn\'t added, status code and error message are correct', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            user: 'Someone Else',
+            password: 'pw123'
+        }
+
+        const result = await api.post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` to be unique')
+        const usersAtEnd = await helper.usersInDb()
+
+        expect(usersAtStart).toHaveLength(usersAtEnd.length)
+    })
+})
+
+describe('creating a new valid user', () => {
+    test('with valid username and password returns 201 created', async () => {
+        const response = await api.post('/api/users/').send({ username: 'Mattikainen', password: 'blabla100' })
+        expect(response.status).toBe(201)
+    })
+})
+
+describe('creating an user with invalid properties', () => {
+    test('empty body returns 400 bad request', async () => {
+        const response = await api.post('/api/users/').send({})
+        expect(response.status).toBe(400)
+    })
+
+    test('throws validation error if username is too short', async () => {
+        const response = await api.post('/api/users/').send({ username: 'Ma', password: 'blabla100' })
+        expect(response.body.error).toContain('`username` (`Ma`) is shorter')
+    })
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
