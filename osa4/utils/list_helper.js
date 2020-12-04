e93861fa94f6a1d20c8a0046ab83b5206f3835a4
Miina Saroma <miinasaroma@gmail.com>
2020-12-04 20:00:50 +0200

    Add functions for most blogs, most likes

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = () => {
     return 1
 }
@@ -20,9 +22,49 @@ const favoriteBlog = (blogs) => {
     })
     return mostLikes
 }
-  
+
+const mostBlogs = (blogs) => {
+    const grouped = _.groupBy(blogs, 'author')
+
+    let mostBlogs = {
+        author: null,
+        blogs: 0
+    }
+
+    for (let key in grouped) {
+        const author = key
+        const blogCount = grouped[key].length
+        if (blogCount >= mostBlogs.blogs) {
+            mostBlogs.author = author
+            mostBlogs.blogs = blogCount
+        }
+    }
+    return mostBlogs
+}
+
+const mostLikes = (blogs) => {
+    const grouped = _.groupBy(blogs, 'author')
+
+    let mostLikes = {
+        author: null,
+        likes: 0
+    }
+
+    for (let key in grouped) {
+        const likes = _.sum(_.map(grouped[key], 'likes'))
+
+        if (likes >= mostLikes.likes) {
+            mostLikes.author = key
+            mostLikes.likes = likes
+        }
+    }
+    return mostLikes
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
