efb47d022e15d4d293ada30b4213112ddc67cdca
Miina Saroma <miinasaroma@gmail.com>
2020-12-09 19:08:22 +0200

    Add LoginForm component

---
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+const LoginForm = ({
+  handleLogin,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password
+}) => {
+  return (
+      <div>
+        <h2>Log in to Bloglist</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => handleUsernameChange(target.value)}
+              />
+            </div>
+            <div>
+            password
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => handlePasswordChange(target.value)}
+              />
+          </div>
+        <button type="submit">login</button>
+        </form>
+      </div>
+    )
+}
+
+export default LoginForm
