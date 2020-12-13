97f84138adcac23a8ef43e4b4a80aa9606af3a40
Miina Saroma <miinasaroma@gmail.com>
2020-12-13 22:46:45 +0200

    Add Notification component

---
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,32 @@
+import React from 'react'
+const Notification = ({ message }) => {    
+    if (!message) {
+        return null
+    }
+
+    const style = {
+        color: "white",
+        backgroundColor: "green",
+        fontFamily: "Arial",
+        padding: "10px",
+        borderStyle: "solid",
+        borderRadius: "5px",
+        marginBottom: "10px",
+    }
+
+    const checkStyle = () => {
+        if (message.includes('fail')) {
+            style.backgroundColor = "Red"
+        }
+        return style
+    }
+
+    return (
+        <div style={checkStyle()}>
+            {message}
+        </div>
+
+    )
+}
+
+export default Notification
