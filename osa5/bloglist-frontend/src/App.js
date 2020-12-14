4842c536f67ac8d43915ffa7a0b7df917d181ef5
Miina Saroma <miinasaroma@gmail.com>
2020-12-14 22:03:01 +0200

    Hide/show BlogForm using Togglable

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
@@ -44,6 +45,7 @@ const App = () => {
   const handlePasswordChange = (value) => {
     setPassword(value)
   }
+
   const addBlog = async (blog) => {
     console.log('createBlog: ', blog)
     try {
@@ -103,7 +105,9 @@ const App = () => {
       <div>
         <div>Logged in as {user.user}</div>
         <button onClick={handleLogout}>log out</button>
-        <BlogForm createBlog={addBlog} />
+        <Togglable buttonLabel={'new blog'}>
+          <BlogForm createBlog={addBlog} />
+        </Togglable>
         <h2>Blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
