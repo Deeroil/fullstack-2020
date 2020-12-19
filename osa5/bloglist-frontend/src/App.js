a9fa48fcda42e29921281947a01337c04322ced4
Miina Saroma <miinasaroma@gmail.com>
2020-12-19 18:04:39 +0200

    Add togglable button for blog details

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -113,7 +113,9 @@ const App = () => {
         </Togglable>
         <h2>Blogs</h2>
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <div key={blog.id}>
+            <Blog blog={blog} />
+          </div>
         )}
       </div>
     )
--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Togglable from './Togglable'
+import BlogDetails from './BlogDetails'
+
 const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
+    <div>
+        <div>
+            {blog.title}, {blog.author}
+        </div>
+        <div>
+            <Togglable buttonLabel={'view'}>
+                <BlogDetails blog={blog} />
+            </Togglable>
+        </div>
+    </div>
+
 )
 
 export default Blog
