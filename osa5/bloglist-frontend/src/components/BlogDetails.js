b5098daa20d4de83d2f5933c92e25727ff891d68
Miina Saroma <miinasaroma@gmail.com>
2020-12-19 17:59:37 +0200

    Add BlogDetails component

---
+++ osa5/bloglist-frontend/src/components/BlogDetails.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const BlogDetails = ({ blog }) => {
+    const user = blog.user
+
+    const detailStyle = {
+            paddingTop: 10,
+            paddingLeft: 2,
+            border: 'solid',
+            borderWidth: 1,
+            marginBottom: 5
+    }
+
+    const handleLike = () => {
+        console.log('Not implemented yet!')
+    }
+
+    return (
+        <div style={detailStyle}>
+            <div>{blog.url}</div>
+            <div>
+                {'likes:' + blog.likes}
+                <button onClick={handleLike}>like</button>
+            </div>
+            <div>{user.user}</div>
+        </div>
+    )
+}
+
+export default BlogDetails
