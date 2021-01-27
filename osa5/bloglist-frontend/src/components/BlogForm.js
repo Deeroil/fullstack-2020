109241f6f315f76d7a05180128ba8c716b41a412
Miina Saroma <miinasaroma@gmail.com>
2021-01-28 00:29:03 +0200

    Add id, rm name from inputs

--- osa5/bloglist-frontend/src/components/BlogForm.js
+++ osa5/bloglist-frontend/src/components/BlogForm.js
@@ -26,13 +26,13 @@ const BlogForm = ({ createBlog }) => {
       <h3>Create a New Blog</h3>
       <form onSubmit={addBlog}>
         <div>
-          title <input type='text' value={title} name='title' onChange={({ target }) => setTitle(target.value)} />
+          title <input id='title' type='text' value={title} onChange={({ target }) => setTitle(target.value)} />
         </div>
         <div>
-          author <input type='text' value={author} name='author' onChange={({ target }) => setAuthor(target.value)} />
+          author <input id='author' type='text' value={author} onChange={({ target }) => setAuthor(target.value)} />
         </div>
         <div>
-          url <input type='text' value={url} name='url' onChange={({ target }) => setUrl(target.value)} />
+          url <input id='url' type='text' value={url} onChange={({ target }) => setUrl(target.value)} />
         </div>
         <button type='submit'>send</button>
       </form>
