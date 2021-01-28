a1e5866f66df3b68d399cca554f87579169b8cbe
Miina Saroma <miinasaroma@gmail.com>
2021-01-28 15:16:50 +0200

    Refactor Blog component

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import Togglable from './Togglable'
-import BlogDetails from './BlogDetails'
-import DeleteButton from './DeleteButton'
 
 const Blog = ({ blog, loggedUser, handleUpdate, handleRemoval }) => {
+  const [detailsVisibility, setDetailsVisibility] = useState(false)
+
+  const toggleVisibility = () => setDetailsVisibility(!detailsVisibility)
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 5,
@@ -16,18 +17,51 @@ const Blog = ({ blog, loggedUser, handleUpdate, handleRemoval }) => {
     marginBottom: 5
   }
 
+  const removeBlog = (event) => {
+    event.preventDefault()
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      handleRemoval(blog)
+    }
+  }
+
+  //only if removal seems allowed
+  const showDeleteButton = () => {
+    return loggedUser.username === blog.user.username
+      ? <button onClick={(e) => removeBlog(e)}>remove</button>
+      : null
+  }
+
+  const addLike = (event) => {
+    event.preventDefault()
+    const updatedBlog = { ...blog, 'likes': blog.likes + 1 }
+    handleUpdate(updatedBlog)
+  }
+
+  const showDetails = () => {
+    return (
+      <div>
+        <div>{blog.url}</div>
+        <div>
+          {'likes:' + blog.likes}
+          <button onClick={(e) => addLike(e)}>like</button>
+        </div>
+        <div>{blog.user.user}</div>
+        {showDeleteButton()}
+      </div>
+    )
+  }
+
   return (
-    <div style={blogStyle}>
+    <div style={blogStyle} className={'blogContent'}>
       <div>
         {blog.title}, {blog.author}
       </div>
       <div>
-        <Togglable buttonLabel={'view'}>
-          <BlogDetails blog={blog} handleUpdate={handleUpdate} />
-          {loggedUser.username === blog.user.username
-            ? <DeleteButton blog={blog} handleRemoval={handleRemoval} />
-            : null}
-        </Togglable>
+        <button onClick={toggleVisibility}> {detailsVisibility ? 'hide' : 'details'} </button>
+        {detailsVisibility
+          ? showDetails()
+          : null
+        }
       </div>
     </div>
   )
